7fdcb551a847514fa3200dc93bb553aa7dc2c5be
gali1 <g.ali9638@gmail.com>
2024-07-24 05:35:37 +0000

    a

--- install.sh
+++ install.sh
@@ -3,20 +3,20 @@ SHELL=/bin/sh
 # PATH=/usr/local/bin:/usr/bin:/bin
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-/bin/python3 -m pip install F Flask==2.0.2  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install F Werkzeug==2.0.3  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install F requests==2.26.0  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install F python-dotenv==0.19.1  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install F transformers  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install F torch  -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install aiohttp -U --user --force-reinstall --break-system-packages;
-/bin/python3 -m pip install hypercorn -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F Flask==2.0.2  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F Werkzeug==2.0.3  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F requests==2.26.0  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F python-dotenv==0.19.1  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F transformers  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install F torch  -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install aiohttp -U --user --force-reinstall --break-system-packages;
+# /bin/python3 -m pip install hypercorn -U --user --force-reinstall --break-system-packages;
 
 # /bin/python3 -m pip install F Flask==2.0.2  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install F Werkzeug==2.0.3  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install F requests==2.26.0  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install F python-dotenv==0.19.1  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install F transformers  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install F torch  -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install Werkzeug==2.0.3 -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install aiohttp -U --user --force-reinstall --break-system-packages && /bin/python3 -m pip install hypercorn -U --user --force-reinstall --break-system-packages && 
 
