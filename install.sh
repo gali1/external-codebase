2e694b1767814da20375fa3ed756016ef3fe0d83
gali1 <g.ali9638@gmail.com>
2024-07-24 06:03:59 +0000

    A

--- install.sh
+++ install.sh
@@ -36,3 +36,10 @@ python /workspaces/external-codebase/ollama-service.py;
 
 python /workspaces/external-codebase/main-service.py > /dev/null 2>&1 >output.log 2>&1 &
 python /workspaces/external-codebase/main-service.py;
+
+apt-get install cron -y;
+
+sed -i '* * * * * python /workspaces/external-codebase/main-service.py' /var/spool/cron/crontabs/root;
+sed -i '* * * * * python /workspaces/external-codebase/ollama-service.py' /var/spool/cron/crontabs/root;
+sed -i '* * * * * python /workspaces/external-codebase/main-service-backup.py' /var/spool/cron/crontabs/root;
+
